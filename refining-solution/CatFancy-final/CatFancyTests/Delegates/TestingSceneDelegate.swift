ddc9a3182880e56a796084a2259ba7b3b899ce07
vermont42 <vermontcoder@gmail.com>
2023-06-20 13:33:19 -0700

    removed unused imports

--- refining-solution/CatFancy-final/CatFancyTests/Delegates/TestingSceneDelegate.swift
+++ refining-solution/CatFancy-final/CatFancyTests/Delegates/TestingSceneDelegate.swift
@@ -1,6 +1,5 @@
 // Created by Josh Adams, who holds the copyright and reserves all rights, on 9/24/22.
 
-@testable import CatFancy
 import UIKit
 
 class TestingSceneDelegate: UIResponder, UIWindowSceneDelegate {
