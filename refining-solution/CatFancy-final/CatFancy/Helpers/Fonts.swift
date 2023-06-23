707854f292126e3b5e08751c15464b1f74a5043d
vermont42 <vermontcoder@gmail.com>
2023-06-22 21:41:37 -0700

    simplify Fonts and Fonts testing

--- refining-solution/CatFancy-final/CatFancy/Helpers/Fonts.swift
+++ refining-solution/CatFancy-final/CatFancy/Helpers/Fonts.swift
@@ -6,11 +6,10 @@ enum Fonts {
   private static let avenirNext = "AvenirNext-"
   private static let demibold = "Demibold"
   private static let bold = "Bold"
-  private static let safeFont = UIFont.systemFont(ofSize: 18.0)
-  static let label = UIFont(name: avenirNext + demibold, size: 18.0) ?? safeFont
-  static let labelBold = UIFont(name: avenirNext + bold, size: 18.0) ?? safeFont
-  static let heading = UIFont(name: avenirNext + bold, size: 24.0) ?? safeFont
-  static let smallBody = UIFont(name: avenirNext + demibold, size: 12.0) ?? safeFont
-  static let buttonSmall = UIFont(name: avenirNext + demibold, size: 18.0) ?? safeFont
-  static let buttonLarge = UIFont(name: avenirNext + demibold, size: 24.0) ?? safeFont
+  static let label = UIFont(name: avenirNext + demibold, size: 18.0)!
+  static let labelBold = UIFont(name: avenirNext + bold, size: 18.0)!
+  static let heading = UIFont(name: avenirNext + bold, size: 24.0)!
+  static let smallBody = UIFont(name: avenirNext + demibold, size: 12.0)!
+  static let buttonSmall = UIFont(name: avenirNext + demibold, size: 18.0)!
+  static let buttonLarge = UIFont(name: avenirNext + demibold, size: 24.0)!
 }
