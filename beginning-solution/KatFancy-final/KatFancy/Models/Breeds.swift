3c7413335e67e18285c6e7a9c17e6fd5e8093a10
vermont42 <vermontcoder@gmail.com>
2023-06-29 14:05:25 -0700

    remake beginning KatFancy using Xcode 15

--- beginning-solution/KatFancy-final/KatFancy/Models/Breeds.swift
+++ beginning-solution/KatFancy-final/KatFancy/Models/Breeds.swift
@@ -1,4 +1,4 @@
-// Created by Josh Adams, who holds the copyright and reserves all rights, on 2/10/23.
+// Created by Josh Adams, who holds the copyright and reserves all rights, on 6/29/23.
 
 struct Breeds: Decodable {
   let breeds: [Breed]
